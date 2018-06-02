1088d8c56badc1b749107095b05044066da5ee13
IrenSverkunova <iren.sverkunova@gmail.com>
2018-06-02 13:47:00 +0300

    refactoring

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 // Components
+import { withProfile } from 'components/HOC/withProfile';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
@@ -12,16 +13,8 @@ import Spinner from 'components/Spinner';
 import Styles from './styles.m.css';
 import { getUniqueID, delay } from 'instruments';
 
+@withProfile
 export default class Feed extends Component {
-    constructor () {
-        super();
-
-        this._createPost = this._createPost.bind(this);
-        this._removePost = this._removePost.bind(this);
-        this._setSpinningState = this._setSpinningState.bind(this);
-        this._likePost = this._likePost.bind(this);
-    }
-
     state = {
         posts: [
             {
@@ -40,7 +33,7 @@ export default class Feed extends Component {
         spinning: false,
     };
 
-    async _removePost (id) {
+    _removePost = async (id) => {
         this._setSpinningState(true);
 
         await delay(600);
@@ -51,13 +44,13 @@ export default class Feed extends Component {
         }));
     }
 
-    _setSpinningState (state) {
+    _setSpinningState = (state) => {
         this.setState({
             spinning: state,
         });
     }
 
-    async _createPost (comment) {
+    _createPost = async (comment) => {
         this._setSpinningState(true);
 
         const post = {
@@ -75,7 +68,7 @@ export default class Feed extends Component {
         }));
     }
 
-    async _likePost (id) {
+    _likePost = async (id) => {
         const { currentUserFirstName, currentUserLastName } = this.props;
 
         this._setSpinningState(true);
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,12 +4,13 @@ import moment from 'moment';
 import { func, string, number, array } from 'prop-types';
 
 // Components
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 import Like from 'components/Like';
 
 // Instruments
 import Styles from './styles.m.css';
 
+@withProfile
 export default class Post extends Component {
     static propTypes = {
         _likePost:   func.isRequired,
@@ -20,39 +21,24 @@ export default class Post extends Component {
         likes:       array.isRequired,
     };
 
-    constructor () {
-        super();
-
-        this.removePost = this._removePost.bind(this);
-    }
-
-    _removePost () {
+    _removePost = () => {
         const { id, _removePost } = this.props;
 
         _removePost(id);
     }
 
     render () {
-        const { comment, created, _likePost, likes, id } = this.props;
+        const { avatar, comment, created, currentUserFirstName, currentUserLastName, _likePost, likes, id } = this.props;
 
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.post }>
-                        <span className = { Styles.cross } onClick = { this.removePost } />
-                        <img src = { context.avatar } />
-                        <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
-                        <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
-                        <p>{comment}</p>
-                        <Like
-                            _likePost = { _likePost }
-                            id = { id }
-                            likes = { likes }
-                            { ...context }
-                        />
-                    </section>
-                )}
-            </Consumer>
+            <section className = { Styles.post }>
+                <span className = { Styles.cross } onClick = { this._removePost } />
+                <img src = { avatar } />
+                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
+                <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
+                <p>{comment}</p>
+                <Like _likePost = { _likePost } id = { id } likes = { likes } />
+            </section>
         );
     }
 }
